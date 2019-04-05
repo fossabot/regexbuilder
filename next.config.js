fa2296e6fa44a04c04fe91fd65c4a7d182a3704f
NieLeben <allescrafterx@gmail.com>
2019-04-05 22:32:10 +0200

    Added sourcemap support and fixed @zeit/next-css

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const path = require("path");
 const withCSS = require("@zeit/next-css");
+const withSourceMaps = require("@zeit/next-source-maps");
 
-module.exports = withCSS({
-  target: "serverless",
-  // eslint-disable-next-line no-unused-vars
-  webpack(config, options) {
-    config.resolve.modules.push(path.resolve("./"));
-    return config;
-  }
-});
+module.exports = withCSS(
+  withSourceMaps({
+    target: "serverless",
+    // eslint-disable-next-line no-unused-vars
+    webpack(config, options) {
+      config.resolve.modules.push(path.resolve("./"));
+      return config;
+    }
+  })
+);
