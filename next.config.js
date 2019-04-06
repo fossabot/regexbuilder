d9f5473216b803fdea1c3e1438922fb3c4946061
NieLeben <allescrafterx@gmail.com>
2019-04-06 19:05:06 +0200

    Added storybook to easy preview react components

--- next.config.js
+++ next.config.js
@@ -8,6 +8,26 @@ module.exports = withCSS(
     // eslint-disable-next-line no-unused-vars
     webpack(config, options) {
       config.resolve.modules.push(path.resolve('./'));
+      config.module.rules.push({
+        test: /\.js$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: options.dev,
+          // eslint options (if necessary)
+        },
+      });
+      config.module.rules.push({
+        test: /\.jsx$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: options.dev,
+          // eslint options (if necessary)
+        },
+      });
       return config;
     },
   })
