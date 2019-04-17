b49003cee4d6f0a020a6ad65456251609d9db2be
Tilmann Meyer <allescrafterx@gmail.com>
2019-04-17 21:33:50 +0200

    refactor(dotenv): Removed dotenv
    Removed dotenv because you add secrets in another way to zeit's now

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,7 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add additional webpack configurations.
-// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
-
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
 const path = require('path');
-const { parsed: localEnv } = require('dotenv').config();
-const webpack = require('webpack');
 
 module.exports = async ({ config, mode }) => {
   config.resolve.modules.push(path.resolve('./'));
-  config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
   config.module.rules.push({
     test: /_\.story\.jsx$/,
     loaders: [require.resolve('@storybook/addon-storysource/loader')],
--- next.config.js
+++ next.config.js
@@ -1,39 +1,30 @@
 const path = require('path');
 const withCSS = require('@zeit/next-css');
-const withSourceMaps = require('@zeit/next-source-maps');
-const { parsed: localEnv } = require('dotenv').config();
-const webpack = require('webpack');
 
-module.exports = withCSS(
-  withSourceMaps({
-    target: 'serverless',
-    // eslint-disable-next-line no-unused-vars
-    webpack(config, options) {
-      config.resolve.modules.push(path.resolve('./'));
-      if (options.dev) {
-        config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
-      }
-      config.module.rules.push({
-        test: /\.js$/,
-        enforce: 'pre',
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-        options: {
-          emitWarning: options.dev,
-          // eslint options (if necessary)
-        },
-      });
-      config.module.rules.push({
-        test: /\.jsx$/,
-        enforce: 'pre',
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-        options: {
-          emitWarning: options.dev,
-          // eslint options (if necessary)
-        },
-      });
-      return config;
-    },
-  })
-);
+module.exports = withCSS({
+  target: 'serverless',
+  webpack(config, options) {
+    config.resolve.modules.push(path.resolve('./'));
+    config.module.rules.push({
+      test: /\.js$/,
+      enforce: 'pre',
+      exclude: /node_modules/,
+      loader: 'eslint-loader',
+      options: {
+        emitWarning: options.dev,
+        // eslint options (if necessary)
+      },
+    });
+    config.module.rules.push({
+      test: /\.jsx$/,
+      enforce: 'pre',
+      exclude: /node_modules/,
+      loader: 'eslint-loader',
+      options: {
+        emitWarning: options.dev,
+        // eslint options (if necessary)
+      },
+    });
+    return config;
+  },
+});
